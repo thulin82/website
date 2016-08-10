fddf51e2fb011201b96fbdd1cb43f632cc485118
Mikael Roos <mikael.t.h.roos@gmail.com>
2016-08-10 10:49:17 +0200

    to separate settings between development and production server for static assets

--- config/url.php
+++ config/url.php
@@ -3,6 +3,13 @@
  * Config file for url.
  * The variable $this is here related to $di.
  */
+
+if (!defined("ANAX_PRODUCTION")) {
+    return [
+        "urlType"       => self::URL_CLEAN,
+    ];
+}
+
 return [
 
     // Defaults to use when creating urls.
